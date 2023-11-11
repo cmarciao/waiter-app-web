1d26986d13a8638f5cb5c613cfd0b38f495b2019
Cássio Marcião <marciaocassio@gmail.com>
2023-11-11 12:13:02 -0300

    feat: Add ingredients hooks methods

--- src/hooks/ingredients.ts
+++ src/hooks/ingredients.ts
@@ -27,3 +27,29 @@ export function useGetAllIngredients() {
 		ingredients: ingredients || []
 	};
 }
+
+export function useUpdateIngredient() {
+	const queryClient = useQueryClient();
+
+	const { isLoading: isUpdatingIngredient, mutateAsync: updateIngredient} = useMutation({
+		mutationFn: ingredientsService.update,
+		onSuccess: () => {
+			queryClient.invalidateQueries(['ingredients']);
+		}
+	});
+
+	return { isUpdatingIngredient, updateIngredient };
+}
+
+export function useRemoveIngredient() {
+	const queryClient = useQueryClient();
+
+	const { isLoading: isRemovingIngredient, mutateAsync: removeIngredient} = useMutation({
+		mutationFn: ingredientsService.remove,
+		onSuccess: () => {
+			queryClient.invalidateQueries(['ingredients']);
+		}
+	});
+
+	return { isRemovingIngredient, removeIngredient };
+}
