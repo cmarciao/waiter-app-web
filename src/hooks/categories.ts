d345f6cc6a7699e342c8ec2f4f987d539cd2232c
Cássio Marcião <marciaocassio@gmail.com>
2023-11-11 12:05:47 -0300

    feat: Add categories hooks methods

--- src/hooks/categories.ts
+++ src/hooks/categories.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { categoriesService } from '@/services/categoriesService';
 
 export function useGetAllCategories() {
@@ -10,4 +10,43 @@ export function useGetAllCategories() {
 	return {
 		categories: categories || []
 	};
+}
+
+export function useCreateCategory() {
+	const queryClient = useQueryClient();
+
+	const {isLoading: isCreatingCategory, mutateAsync: createCategory} = useMutation({
+		mutationFn: categoriesService.create,
+		onSuccess: () => {
+			queryClient.invalidateQueries(['categories']);
+		}
+	});
+
+	return { isCreatingCategory, createCategory };
+}
+
+export function useUpdateCategory() {
+	const queryClient = useQueryClient();
+
+	const { isLoading: isUpdatingCategory, mutateAsync: updateCategory } = useMutation({
+		mutationFn: categoriesService.update,
+		onSuccess: () => {
+			queryClient.invalidateQueries('categories');
+		}
+	});
+
+	return { updateCategory, isUpdatingCategory };
+}
+
+export function useRemoveCategory() {
+	const queryClient = useQueryClient();
+
+	const { isLoading: isRemovingCategory, mutateAsync: removeCategory } = useMutation({
+		mutationFn: categoriesService.remove,
+		onSuccess: () => {
+			queryClient.invalidateQueries(['categories']);
+		}
+	});
+
+	return { isRemovingCategory, removeCategory };
 }
