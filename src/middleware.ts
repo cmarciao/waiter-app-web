bc9fb0853b0e90689be2067bd661db165bfc673f
Cássio Marcião <marciaocassio@gmail.com>
2024-07-07 01:06:53 -0300

    feat: Update the middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,52 +13,54 @@ import { cookiesNames } from '@/constants/cookies-names';
 export default async function middleware(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 
-	if(checkIsDefaultNextRoute(pathname)) {
+	if (checkIsDefaultNextRoute(pathname)) {
 		return NextResponse.next();
 	}
 
 	const isPublicRoute = checkIsPublicRoute(pathname);
 	const accessToken = cookies().get(cookiesNames.accessToken)?.value;
 
-	const response = await fetch(`${basePath}/auth/is-access-token-valid`, {
-		headers: {
-			'Authorization': `Bearer ${accessToken}`
-		}
-	});
+	try {
+		const response = await fetch(`${basePath}/auth/is-access-token-valid`, {
+			headers: {
+				'Authorization': `Bearer ${accessToken}`
+			}
+		});
 
-	if(accessToken) {
-		if(!isPublicRoute) {
-			const middlewareResponse = NextResponse.next();
+		if (accessToken) {
+			if (!isPublicRoute) {
+				const middlewareResponse = NextResponse.next();
 
-			if(response.status === 401) {
-				const { accessToken, refreshToken: refreshTokenId } = await refreshToken();
+				if (response.status === 401) {
+					const { accessToken, refreshToken: refreshTokenId } = await refreshToken();
 
-				middlewareResponse.cookies.set({
-					name: cookiesNames.accessToken,
-					value: accessToken,
-					httpOnly: true
-				});
+					middlewareResponse.cookies.set({
+						name: cookiesNames.accessToken,
+						value: accessToken,
+						httpOnly: true
+					});
 
-				middlewareResponse.cookies.set({
-					name: cookiesNames.refreshToken,
-					value: refreshTokenId,
-					httpOnly: true
-				});
+					middlewareResponse.cookies.set({
+						name: cookiesNames.refreshToken,
+						value: refreshTokenId,
+						httpOnly: true
+					});
+				}
+
+				return middlewareResponse;
 			}
 
-			middlewareResponse.headers.append('test', 'test');
-
-			return middlewareResponse;
+			const url = new URL(APP_ROUTES.private.home, req.nextUrl.origin);
+			return NextResponse.redirect(url);
 		}
-
-		const url = new URL(APP_ROUTES.private.home, req.nextUrl.origin);
-		return NextResponse.redirect(url);
-	} else {
-		if(isPublicRoute && pathname !== '/') return NextResponse.next();
-
-		const url = new URL(APP_ROUTES.public.signIn, req.nextUrl.origin);
-		return NextResponse.redirect(url);
+	} catch {
+		// Do nothing
 	}
+
+	if (isPublicRoute && pathname !== '/') return NextResponse.next();
+
+	const url = new URL(APP_ROUTES.public.signIn, req.nextUrl.origin);
+	return NextResponse.redirect(url);
 }
 
 export const config = {
